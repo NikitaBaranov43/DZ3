d980a8f7c037923877f34b94729caf1cc3ee8452
NikitaBaranov43 <fast.flou@gmail.com>
2024-07-28 18:48:04 +0300

    Add files via upload

--- DZ3.py
+++ DZ3.py
@@ -3,4 +3,4 @@ number_of_hours_spent = '1.5,'
 course_name = 'Python,'
 time_for_one_task = 0
 time_for_one_task = .125
-print (course_name, "всего задач:12", 'затрачено часов:', number_of_hours_spent , 'среднее время выполнения ' , time_for_one_task,"часов")
+print (course_name, "всего задач:", number_of_completed_HW, 'затрачено часов:', number_of_hours_spent , 'среднее время выполнения ' , time_for_one_task,"часов")
